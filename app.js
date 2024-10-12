8ed40fd146bb07e165635dd16561fc351ab9b088
Hardi-Kun <m.hardiansyah0406@gmail.com>
2024-10-12 11:44:06 +0700

    bugs setelah login

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const express       = require('express')
 const app           = express()
 const port          = 4000
+const cookieParser  = require ('cookie-parser')
+const session       = require ('express-session')
 
 const c_beranda     = require ('./controller/c_beranda')
 const c_auth        = require ('./controller/c_auth')
 const c_toko        = require ('./controller/c_toko')
+const cek_login     = c_auth.cek_login
 
+// settingan untuk data sessio  login
+app.use( cookieParser('rahasia') )
+app.use( session({
+    secret: 'rahasia',
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: (1000 * 60) * 30
+        // batas session expired:
+        // 1000 milidetik * 60 = 1 menit
+        // 1 menit * 30 = 1/2 jam
+    }
+}))
 
 app.use( express.urlencoded({extended:false}))
 app.use( express.static('public'))
@@ -17,7 +33,7 @@ app.get('/', c_beranda.beranda)
 app.get('/auth/login', c_auth.hal_login)
 app.post('/auth/proses-login' , c_auth.proses_login)
 
-app.get('/toko', c_toko.index)
+app.get('/toko', cek_login, c_toko.index)
 
 
 
--- controller/c_auth.js
+++ controller/c_auth.js
@@ -4,24 +4,28 @@ const m_user        = require ('../model//m_user')
 module.exports = 
 {
     hal_login: (req,res) => {
-        let data = {
-            notifikasi: req.query.notif,
+        if (req.session.user) {
+            res.redirect('/toko')
+        } else {
+            let data = {
+                notifikasi: req.query.notif,
+            }
+            res.render('v_auth/login', data)
         }
-        res.render('v_auth/login', data)
     },
 
     proses_login: async (req,res) => {
         // ambil inputan dari form login
         let form_email       = req.body.form_email
         let form_password    = req.body.form_password
-        // console.log(form_password)
+
         // cek email yg dinput, ada gak di db
         let email_exist = await m_user.cari_email (form_email)
         if (email_exist.length > 0) {
             // cek password
             let password_cocok = bcrypt.compareSync(form_password, email_exist[0].password)
-            // console.log(password_cocok)
             if (password_cocok) {
+                req.session.user = email_exist
                 res.redirect('/toko')
             } else {
                 let pesan = 'Password Salah!'
@@ -33,5 +37,16 @@ module.exports =
             res.redirect(`/auth/login?notif=${pesan}`)
         }
 
-    }
+    },
+
+    cek_login: function(req,res,next) {
+        if (req.session.user) {
+            next()
+        } else {
+            // lempar ke halaman login
+            let pesan = `Sesi anda habis! silakan login dulu`
+            res.redirect(`/auth/login?notif=${pesan}`)
+        }
+    },
+
 }
