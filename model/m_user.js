8fadf12f57460f7abee570d10136ae054ff67a44
Hardi-Kun <m.hardiansyah0406@gmail.com>
2024-10-14 19:19:16 +0700

    cek login, perbaikan css login, & fontawesome

--- model/m_user.js
+++ model/m_user.js
@@ -1,6 +1,5 @@
 const mysql               = require ('mysql2')
 const config_database     = require ('../config/database')
-const res = require('express/lib/response')
 const db                  = config_database.db
 const eksekusi            = config_database.eksekusi
 
