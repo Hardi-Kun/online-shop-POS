0201bb58a5db845612b0a703ff9e724843767c1c
Hardi-Kun <m.hardiansyah0406@gmail.com>
2024-10-18 19:53:27 +0700

    list kategori produk

--- controller/c_olshop.js
+++ controller/c_olshop.js
@@ -1,8 +1,14 @@
+const m_master_produk_kategori = require('../model/m_master_produk_kategori')
+const m_prod_kategori = require ('../model/m_master_produk_kategori')
+
 module.exports = 
 {
 
-    hal_beranda: (req,res) => {
-        res.render('v_olshop/beranda')
+    hal_beranda: async (req,res) => {
+        let data = {
+            kategoriProduk: await m_master_produk_kategori.getSemua()
+        }
+        res.render('v_olshop/beranda', data)
     }
 
 }
